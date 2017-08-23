06edad0b8dac25a0faafa10ef374f730a663d180
Brock Pettyjohn <brockpettyjohn@gmail.com>
2017-08-23 12:15:43 -0600

    basic messages displaying

--- my-app/src/components/messages/header/Message_Header.jsx
+++ my-app/src/components/messages/header/Message_Header.jsx
@@ -1,38 +1,60 @@
 import React, { Component } from 'react';
 import '../Message_Page.css';
 
-class MessageHeader extends Component{
-    constructor(){
+class MessageHeader extends Component {
+    constructor() {
         super();
 
         this.state = {
             input: '',
-            message: ''
+            message: []
         }
         this.handleClick = this.handleClick.bind(this)
+        this.handleInputChange = this.handleInputChange.bind(this)
     }
-        
-        
-        handleInputChange(event){
-            this.setState({ input: (event.target.value) })
-        }
-        handleClick() {
-            this.setState({
-                message: this.state.input,
-                input: ''
-            })
-        }
-    render(){
-        console.log(this.state)
+
+    handleInputChange(event) {
+        this.setState({ input: (event.target.value) })
+    }
+
+    handleKeyPress() {
+        const newMessage = this.state.message.concat(this.state.input) 
+         this.setState({
+            message: newMessage,
+            input: ''
+        })
+    }
+
+    handleClick() {
+        const newMessage = this.state.message.concat(this.state.input)
+        // const newMessage = this.state.message
+        // newMessage.push(this.state.input)  in this scenario in order to us push you have to set it equal to a variable and push into the var because push modifies the original array and you cannot modify state directly
+        this.setState({
+            message: newMessage,
+            input: ''
+        })
+    }
+    render() {
         return (
             <div className='searchbar'>
-                <input 
-                    name='searchbar' 
+                <input
+                    name='searchbar'
                     placeholder='Search'
-                    onChange={ event => this.handleInputChange( event )} 
-                    value={ this.state.input }/>
-                    <button onClick= { this.handleClick }>Add</button>
-                <span>{ this.state.message }</span>
+                    onChange={this.handleInputChange}
+                    value={this.state.input}
+                    onKeyPress={(e) => {
+                            if (e.key === 'Enter') {
+                            this.handleClick();
+                        }
+                    }
+                    } />
+                <button onClick={this.handleClick}>Add</button>
+                <div>{
+                    this.state.message.map((message, i) => {
+                        return <p key={i}>{message}</p>
+                    })
+                }
+                </div>
             </div>
         )
     }
