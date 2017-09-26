48f953754ee55628d7a4419cfeb558652a4b0eb6
brockpettyjohn <brockpettyjohn@gmail.com>
2017-09-26 07:42:34 -0600

    double checking

--- my-app/src/components/messages/header/Message_Header.jsx
+++ my-app/src/components/messages/header/Message_Header.jsx
@@ -46,13 +46,13 @@ class MessageHeader extends Component {
                     placeholder='Search'
                     onChange={e => handleInputChange(e.target.value)}
                     value={this.props.input}
-                    onKeyPress={ e => {
+                    onKeyPress={e => {
                         if (e.key === 'Enter') {
                             handleClick(this.props.input);
                         }
                     }
                     } />
-                <button onClick={ e => handleClick(this.props.input)}>Add</button>
+                <button onClick={e => handleClick(this.props.input)}>Add</button>
                 <div>{
                     this.props.message.map((message, i) => {
                         return <p key={i}>{message}</p>
